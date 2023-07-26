3304a88c694131e249ed7c371c63524e2493ef18
Mark Chen <markchen555@gmail.com>
2023-07-26 00:59:59 -0700

    [FE] finished the basic layout of footer

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,10 @@
+import { NavLinks } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
+import AuthProviders from "./AuthProviders";
 
 const Navbar = () => {
+  const session = { user: "test" };
   return (
     <nav className="flexBetween navbar">
       <div className="flex-1 flexStart gap-10">
@@ -14,6 +17,22 @@ const Navbar = () => {
             alt="Protibble"
           />
         </Link>
+        <ul className="xl:flex hidden text-small gap-7">
+          {NavLinks.map((link) => (
+            <Link href={link.href}>{link.text}</Link>
+          ))}
+        </ul>
+      </div>
+
+      <div className="flexCener gap-4">
+        {session?.user ? (
+          <>
+            UserPhoto
+            <Link href="/create-project">Share Work</Link>
+          </>
+        ) : (
+          <AuthProviders></AuthProviders>
+        )}
       </div>
     </nav>
   );
