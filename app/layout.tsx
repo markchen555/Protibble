f2d5d0d8d999cee8157834f5657a21e2ce36ad71
Mark Chen <markchen555@gmail.com>
2023-07-29 23:37:47 -0700

    [FE] add fix to remove extra attribute warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,18 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+// import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Portibble",
   description: "Showcase and discover awesome developer projects",
 };
 
+// https://www.slingacademy.com/article/next-js-warning-extra-attributes-from-the-server/
+// use suppressHydrationWarning to remove Extra attributes Warning
 export default function RootLayout({
   children,
 }: {
@@ -18,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body suppressHydrationWarning={true}>
         <Navbar />
         <main>{children}</main>
         <Footer />
