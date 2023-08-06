e97d874d05529394b1836f926ede75f0d9b8700f
Mark Chen <markchen555@gmail.com>
2023-08-06 03:00:59 -0700

    [FE] testing grafbase deployment

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -8,6 +8,7 @@ const User = g.model('User', {
   description: g.string().optional(),
   githubUrl: g.url().optional(),
   linkedinUrl: g.url().optional(),
+  //@ts-ignore
   projects: g.relation(() => Project).list().optional(), // each use can relation to many projects, initially can be zero so its optional
 }).auth((rules) => {
   rules.public().read() // everyone can read the users
